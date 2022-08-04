9dda54c9f29479b24eecc8730319c6266dc51007
Henri <lefebvre.hp@gmail.com>
2022-08-04 14:48:02 +0200

    Updated for with loginhelper

--- bot.js
+++ bot.js
@@ -1,9 +1,19 @@
 console.log("  _  _      _    _         _   ", "\n", "| || |__ _| |__| |__  ___| |_ ", "\n", "| __ / _` | '_ \\ '_ \\/ _ \\  _|", "\n", "|_||_\\__,_|_.__/_.__/\\___/\\__|", "\n");
 
 const args = process.argv.splice(2);
-if (args.length < 1) {
-    console.log("Usage: node bot.js <hotel> [<sso>]");
-    console.log("Example: node bot.js fr hhfr.abcdefg-1234-abcd-1234-abcdefghijk-12345678.V4");
+if (args.length <= 1) {
+    console.log("Usage: node bot.js <hotel> [options]");
+    console.log("Options:");
+    console.log(" == SSO ticket method ==");
+    console.log("  bot.sso <ssoticket> Provide a valid SSO ticket to login with");
+    console.log("  Example: node bot.js fr sso hhfr.abcdefg-1234-abcd-1234-abcdefghijk-12345678.V4");
+    console.log("\n == Simple login method (could need captcha validation) ==");
+    console.log("  bot.email <email> Your account email");
+    console.log("  bot.password <password> Your account password");
+    console.log("  bot.username <username> The username of the avatar you want to use");
+    console.log("  Example: node bot.js fr email youremail@gmail.com password yourpassword username yourusername");
+    //console.log("\n == Multiple login method (could need captcha validation) ==");
+    //console.log("  bot.credsFile <path> Path to a file containing your account credentials");
     process.exit(1);
 }
 
@@ -13,6 +23,7 @@ const dom = new JSDOM(`<!DOCTYPE html><p>Bot test</p>`);
 window = dom.window;
 
 const Client = require("./src/Client.js");
+const LoginHelper = require("./src/Login/LoginHelper.js");
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
@@ -20,20 +31,45 @@ const readline = require('readline').createInterface({
 
 console.log("✅");
 
-if (args.length == 1) {
-    readline.question("🔐 Enter your SSO ticket: ", (sso) => {
-        process.stdout.write("⏳ Starting the bot... ");
-
-        let bot = new Client(args[0], sso.replace("hhfr.", ""));
-        bot.connect();
-
-        console.log("✅");
-    });
-}else {
-    process.stdout.write("⏳ Starting the bot... ");
+let bot;
+switch (args[1]) {
+    case "bot.sso":
+        readline.question("🔐 Enter your SSO ticket: ", (sso) => {
+            process.stdout.write("⏳ Starting the bot... ");
     
-    let bot = new Client(args[0], args[1].replace("hhfr.", ""));
-    bot.connect();
+            bot = new Client(args[0], sso.replace("hhfr.", ""));
+            bot.connect();
+    
+            console.log("✅");
+        });
+    break;
+    case "bot.credsFile":
+        console.log("🛠 Sorry, this feature is not yet implemented");
+        process.exit(1);
+    default:
+        if (args.length < 7) {
+            console.log("⚠️ Not enough arguments");
+            process.exit(1);
+        }
 
-    console.log("✅");
+        console.log(args);
+
+        const email = args[args.indexOf("bot.email") + 1];
+        const password = args[args.indexOf("bot.password") + 1];
+        const username = args[args.indexOf("bot.username") + 1];
+
+        const login = new LoginHelper(args[0]);
+        login.getSimpleTicket(email, password, username, (ticket) => {
+            if (!ticket) {
+                console.log("⚠️ Failed to get ticket");
+                process.exit(1);
+            }
+    
+            process.stdout.write("⏳ Starting the bot... ");
+    
+            bot = new Client(args[0], ticket.replace("hhfr.", ""));
+            bot.connect();
+    
+            console.log("✅");
+        });
 }
